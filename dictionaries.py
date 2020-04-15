fbd65b45a7175414188334a137adfad631402a08
ceguja <ceguja@gmail.com>
2020-04-15 10:21:19 -0500

    initial

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+ages = {"Alice": 22, "Bob": 27}
+ages["Charlie"] = 30
+ages["Alice"] += 1
+
+print(ages)
+
+for age in ages:
+    print(age)
---
+++ sequences.py
@@ -0,0 +1,8 @@
+name = "Alice"
+coordinates = (10.0, 20.0)
+names = ["Alice","Bob", "Charlie"]
+
+print(name[1])
+print(coordinates[1])
+print(names[1])
+print(names[2])
